82024882c4ea1490632db05a3280c11b03a17d79
Shannen Leong <shannenleongxe@gmail.com>
2022-02-15 21:00:21 +0800

    Initial populate groups page

---
+++ lib/groups_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class GroupsPage extends StatefulWidget {
+  const GroupsPage({Key? key}) : super(key: key);
+
+  @override
+  _GroupsPageState createState() => _GroupsPageState();
+}
+
+class _GroupsPageState extends State<GroupsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GROUPS'),
+      ),
+    );
+  }
+}
