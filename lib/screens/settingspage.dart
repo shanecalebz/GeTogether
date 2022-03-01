e1d5a75a0dfdc7895089b65cb4ef988a7ad69f8c
Li Hao <lihao288@hotmail.com>
2022-03-02 04:19:17 +0800

    added settings page dart file

---
+++ lib/screens/settingspage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState () => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: SafeArea(
+      child: ListView(
+        padding: EdgeInsets.all(24.0),
+        children: [],
+      ),
+    ),
+  );
+}
