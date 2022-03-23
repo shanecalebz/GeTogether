7564d8b2d89142fbd5ed0842324169b73d54cf35
Jeff Soh <jeffswr9320@gmail.com>
2022-03-23 16:07:24 +0800

    Added logout button

--- lib/screens/settingspage.dart
+++ lib/screens/settingspage.dart
@@ -2,64 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:getogether/widgets/icon_widget.dart';
 
+import '../Authenticate/Methods.dart';
+
 class SettingsPage extends StatefulWidget {
   @override
-  _SettingsPageState createState () => _SettingsPageState();
+  _SettingsPageState createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: SafeArea(
-      child: ListView(
-        padding: EdgeInsets.all(24.0),
-        children: [
-          SettingsGroup(
-            title: 'GENERAL',
-            children: <Widget>[
-              buildLogout(),
-              buildDeleteAccount(),
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.all(24.0),
+            children: [
+              SettingsGroup(
+                title: 'GENERAL',
+                children: <Widget>[
+                  buildLogout(),
+                  buildDeleteAccount(),
+                ],
+              ),
+              const SizedBox(height: 32),
+              SettingsGroup(
+                title: 'FEEDBACK',
+                children: <Widget>[
+                  const SizedBox(height: 8),
+                  buildReportBug(),
+                  buildSendFeedback(),
+                ],
+              ),
             ],
           ),
-          const SizedBox(height: 32),
-          SettingsGroup(
-            title: 'FEEDBACK',
-            children: <Widget>[
-              const SizedBox(height: 8),
-              buildReportBug(),
-              buildSendFeedback(),
-            ],
-          ),
-        ],
-      ),
-    ),
-  );
+        ),
+      );
 
-  Widget buildLogout() => SimpleSettingsTile(
-    title: 'Logout',
-    subtitle: '',
-    leading: IconWidget(icon: Icons.logout, color: Colors.blueAccent),
-    onTap: () {},
-  );
+  Widget buildLogout() =>
+      IconButton(icon: Icon(Icons.logout), onPressed: () => logOut(context));
 
   Widget buildDeleteAccount() => SimpleSettingsTile(
-    title: 'Delete Account',
-    subtitle: '',
-    leading: IconWidget(icon: Icons.delete, color: Colors.pink),
-    onTap: () {},
-  );
+        title: 'Delete Account',
+        subtitle: '',
+        leading: IconWidget(icon: Icons.delete, color: Colors.pink),
+        onTap: () {},
+      );
 
   Widget buildReportBug() => SimpleSettingsTile(
-    title: 'Report A Bug',
-    subtitle: '',
-    leading: IconWidget(icon: Icons.bug_report, color: Colors.teal),
-    onTap: () {},
-  );
+        title: 'Report A Bug',
+        subtitle: '',
+        leading: IconWidget(icon: Icons.bug_report, color: Colors.teal),
+        onTap: () {},
+      );
 
   Widget buildSendFeedback() => SimpleSettingsTile(
-    title: 'Send Feedback',
-    subtitle: '',
-    leading: IconWidget(icon: Icons.thumb_up, color: Colors.purple),
-    onTap: () {},
-  );
+        title: 'Send Feedback',
+        subtitle: '',
+        leading: IconWidget(icon: Icons.thumb_up, color: Colors.purple),
+        onTap: () {},
+      );
 }
