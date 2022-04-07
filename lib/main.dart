7b542e731b0cdea2a319dbfa3745a515f42756d4
lihao <lihao288@hotmail.com>
2022-04-07 15:57:11 +0800

    correct spelling error of authenticate directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:getogether/Authenticate/Autheticate.dart';
+import 'package:getogether/Authenticate/Authenticate.dart';
 import 'package:getogether/Authenticate/LoginScreen.dart';
 
 void main() async {
