660213564bda403d54feff1a8b2998346f1bf25c
lihao <lihao288@hotmail.com>
2022-04-09 13:52:46 +0800

    Fixed checkboxes in Notifs, added timestamps for each tx

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
+import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:getogether/utils/constants.dart';
-import 'package:getogether/widgets/custom_app_bar.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,15 +13,34 @@ class _HomeScreenState extends State<HomeScreen> {
 
   int count = 0;
   int index = 0;
+  late int checkBoxValueIndex;
+  late bool onlyOwner;
+  late Timer timer;
   bool currentUserInGroup = false;
   List<String> userList = [];
+  List<String> checkBoxValue = [];
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   FirebaseAuth _auth = FirebaseAuth.instance;
 
+  void startTimer() {
+    timer = Timer.periodic(const Duration(milliseconds: 2000), (Timer timer) {
+      setState(() {
+        getNotifications();
+      });
+      timer.cancel();
+    });
+  }
+
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    timer.cancel();
+  }
+
+  void getNotifications() {
     userList.clear();
+    checkBoxValue.clear();
     index = 0;
     FirebaseFirestore _firestore = FirebaseFirestore.instance;
     _firestore.collection('notifications').get().then((value) => value.docs.forEach((element) {
@@ -37,12 +56,95 @@ class _HomeScreenState extends State<HomeScreen> {
       if (currentUserInGroup == true) {
         for (String users in element.data()['test'].split(';')) {
           setState(() {
-            userList.add(index.toString() + "," + users + element.id);
+            userList.add(index.toString() + "," + users + "," + element.id + "," + element.data()['eventTime']);
+            if (_auth.currentUser!.uid == userList.last.split(',')[1] && userList.last.split(',')[5] == "no" && userList.last.split(',')[4] == "no") {
+              checkBoxValue.add((userList.length - 1).toString() + ",false");
+            }
           });
         }
         index++;
       }
-      }));
+    }));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      getNotifications();
+    });
+  }
+
+  Widget buildCheckbox(int j) {
+
+    checkBoxValueIndex = -1;
+    for (int i = 0; i < checkBoxValue.length; i++) {
+     if (checkBoxValue[i].split(',')[0] == j.toString()) {
+       checkBoxValueIndex = i;
+     }
+    }
+
+    if (checkBoxValueIndex != -1) {
+      return Checkbox(
+          value: checkBoxValue[checkBoxValueIndex].split(',')[1].toString() == "true",
+          onChanged: (value) async {
+            // UPDATE FIRESTORE
+            String temp = "";
+            for (int z = 0; z < userList.length; z++) {
+              if (userList[z].split(',')[0] == userList[j].split(',')[0]) {
+                if (_auth.currentUser!.uid != userList[z].split(',')[1]) {
+                  temp += userList[z].split(',')[1] + "," + userList[z].split(',')[2] + "," + userList[z].split(',')[3] + "," +
+                      userList[z].split(',')[4] + "," + userList[z].split(',')[5] + ";";
+                }
+              }
+            }
+            temp = temp.substring(0, temp.length - 1);
+
+            // CHECK IF OWNER ONLY
+            onlyOwner = true;
+            for (String user in temp.split(';')) {
+              if (user.split(',')[4] == "no") {
+                onlyOwner = false;
+              }
+            }
+
+            if (onlyOwner == true) {
+              // DELETE DOCUMENT
+              await _firestore.collection('notifications').doc(userList[j].split(',')[6]).delete();
+            } else {
+              // UPDATE DOCUMENT
+              await _firestore.collection('notifications').doc(userList[j].split(',')[6]).update({'test': temp});
+            }
+
+            // SHOW SNACKBAR
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text(
+                  "Settled",
+                  style: TextStyle(
+                    color: Colors.white,
+                  )
+              ),
+              duration: Duration(seconds: 3),
+              backgroundColor: Color(0XFFFEA828),
+            ));
+
+            // SET CHECKBOX VALUE TO TRUE
+            setState(() {
+              for (int i = 0; i < checkBoxValue.length; i++) {
+                if (checkBoxValue[i].split(',')[0] == j.toString()) {
+                  checkBoxValue[i] = checkBoxValue[i].split(',')[0] + ",true";
+                }
+              }
+            });
+
+            // START TIMER
+            startTimer();
+          }
+      );
+    } else {
+      return Container();
+    }
   }
 
   Widget buildYouOwe() {
@@ -60,7 +162,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     if (count == 0) {
       return const Center(
-        child: Text("No Outstanding")
+        child: Text("Bills Settled")
       );
     } else {
       return SingleChildScrollView(
@@ -91,7 +193,18 @@ class _HomeScreenState extends State<HomeScreen> {
                               children: [
                                 for (int i = 0; i < userList.length; i++)
                                   if (userList[i].split(',')[0] == userList[j].split(',')[0] && userList[i].split(',')[5] == "yes")
-                                    Text(userList[i].split(',')[2]),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          userList[i].split(',')[2],
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(DateFormat('dd MMM yyyy HH:mm').format(DateTime.fromMillisecondsSinceEpoch(int.parse(userList[i].split(',')[7].toString())))),
+                                      ],
+                                    ),
                                 Row(
                                   children: [
                                     Padding(
@@ -101,25 +214,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                     SizedBox(
                                       width: 20.0,
                                       height: 20.0,
-                                      child: Checkbox(
-                                          value: false,
-                                          onChanged: (value) {
-                                            // UPDATE FIRESTORE
-
-
-                                            // SHOW SNACKBAR
-                                            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                                              content: Text(
-                                                "Item Deleted",
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                )
-                                              ),
-                                              duration: Duration(seconds: 3),
-                                              backgroundColor: Color(0XFFFEA828),
-                                            ));
-                                          }
-                                      ),
+                                      child: buildCheckbox(j),
                                     ),
                                   ],
                                 )
@@ -152,7 +247,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     if (count == 0) {
       return const Center(
-        child: Text("NOBODY OWES U SHIT")
+        child: Text("Bills Settled")
       );
     } else {
       return SingleChildScrollView(
@@ -183,7 +278,18 @@ class _HomeScreenState extends State<HomeScreen> {
                                   child: Row(
                                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text(userList[i].split(',')[2]),
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            userList[i].split(',')[2],
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          Text(DateFormat('dd MMM yyyy HH:mm').format(DateTime.fromMillisecondsSinceEpoch(int.parse(userList[i].split(',')[7].toString())))),
+                                        ],
+                                      ),
                                       Text("\$" + userList[j].split(',')[3]),
                                     ],
                                   ),
@@ -201,10 +307,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -224,50 +326,6 @@ class _HomeScreenState extends State<HomeScreen> {
             buildTheyOwe(),
           ],
         )
-      ),
-    );
-  }
-
-  SliverToBoxAdapter _buildHeader(double screenHeight) {
-    return SliverToBoxAdapter(
-      child: Container(
-        padding: const EdgeInsets.all(20.0),
-        decoration: BoxDecoration(
-          color: Palette.primaryColor,
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(40.0),
-            bottomRight: Radius.circular(40.0),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text(
-                  'UPCOMING EVENTS',
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: screenHeight * 0.01),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Your upcoming plans:',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
       ),
     );
   }
